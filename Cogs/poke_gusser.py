be31c9faa6ac20642658a851b8131fc679272d4b
Neko Girl <166271770+cringe-neko-girl@users.noreply.github.com>
2024-06-06 22:46:36 -0400

    Update poke_gusser.py

--- Cogs/poke_gusser.py
+++ Cogs/poke_gusser.py
@@ -56,67 +56,165 @@ class PokemonPredictor(commands.Cog):
                         logger.error(f"Failed to fetch information for the Pokémon {pokemon_name}.")
                 else:
                     logger.info(f"Image for {pokemon_name} already exists, skipping download.")
-
-    async def predict_pokemon(self, ctx, img, threshold=0.8):
-     try:
-        logger.debug("Predicting Pokémon from provided image...")
-        async with ctx.typing():  # Activate typing indicator
-            if not os.path.exists(self.image_folder):
-                os.makedirs(self.image_folder)
-
-            pokemon_files = os.listdir(self.image_folder)
-            logger.debug(f"Number of Pokémon images found: {len(pokemon_files)}")
-
-            best_match = None
-            highest_score = 0
-
-            img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-            img = cv2.resize(img, (224, 224))
-
-            orb = cv2.ORB_create()
-
-            keypoints1, descriptors1 = orb.detectAndCompute(img, None)
-
-            for pokemon_file in pokemon_files:
-                pokemon_name, _ = os.path.splitext(pokemon_file)
-                stored_img_path = os.path.join(self.image_folder, pokemon_file)
-                stored_img = Image.open(stored_img_path)
-                stored_img = stored_img.convert("RGBA") if stored_img.mode == "P" else stored_img
-                stored_img = stored_img.resize((224, 224)).convert('RGB')
-                stored_img = cv2.cvtColor(np.array(stored_img), cv2.COLOR_RGB2BGR)
-
-                keypoints2, descriptors2 = orb.detectAndCompute(stored_img, None)
-
-                if descriptors1 is not None and descriptors2 is not None:
-                    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-                    matches = bf.match(descriptors1, descriptors2)
-                    matches = sorted(matches, key=lambda x: x.distance)
-
-                    if matches:
-                        score = len(matches) / len(descriptors1)
-                        logger.debug(f"Matching score for {pokemon_name}: {score:.2%}")
-
-                        if score > highest_score:
-                            highest_score = score
-                            best_match = pokemon_name
-
-            if highest_score >= threshold:
-                logger.info(f"Best match: {best_match} with score {highest_score:.2%}")
-            else:
-                logger.info("No match found above the threshold.")
-                
-            return best_match, highest_score
-
-     except Exception as e:
-        logger.error(f"An error occurred during prediction: {e}")
-        return None, 0
+  
+    def remove_srgb_profile(self, img_path):
+        try:
+            with Image.open(img_path) as img:
+                img.save(img_path, icc_profile=None)
+                logger.debug(f"Removed sRGB profile from {img_path}")
+        except Exception as e:
+            logger.error(f"Error removing sRGB profile: {e}")
+            
+    def ensure_correct_color_format(self, img):
+        """
+        Convert image from BGR to RGB if needed to ensure consistent color format.
+        """
+        if img.shape[2] == 3:  # Check if the image has 3 color channels
+            return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        return img
     
-    def calculate_ssim(self, img1, img2):
-     img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-     img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-     ssim_score, _ = ssim(img1_gray, img2_gray, full=True)
-     logger.debug(f"Calculated SSIM score: {ssim_score:.2%}")
-     return ssim_score
+    
+    async def predict_pokemon(self, ctx, img, threshold=0.8):
+        try:
+            logger.debug("Predicting Pokémon from provided image...")
+            async with ctx.typing():
+                if not os.path.exists(self.image_folder):
+                    os.makedirs(self.image_folder)
+
+                pokemon_files = os.listdir(self.image_folder)
+                logger.debug(f"Number of Pokémon images found: {len(pokemon_files)}")
+
+                best_match = None
+                highest_score = float('-inf')  # Initialize with a very low value
+
+                # Convert image to numpy array and ensure correct color format
+                img_np = np.array(img)
+                img_np = self.ensure_correct_color_format(img_np)
+
+                # Convert image to binary for contour detection
+                gray_img = cv2.cvtColor(img_np, cv2.COLOR_RGB2GRAY)
+                _, binary_img = cv2.threshold(gray_img, 127, 255, cv2.THRESH_BINARY)
+
+                # Find contours
+                contours, _ = cv2.findContours(binary_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                # Initialize variables to store the largest contour's index and area
+                largest_contour_idx = -1
+                largest_contour_area = 0
+
+                for idx, contour in enumerate(contours):
+                    # Calculate contour area
+                    contour_area = cv2.contourArea(contour)
+
+                    # Check if contour area is greater than the current largest contour area
+                    if contour_area > largest_contour_area:
+                        largest_contour_idx = idx
+                        largest_contour_area = contour_area
+
+                # Only proceed if a valid contour is found
+                if largest_contour_idx != -1:
+                    x, y, w, h = cv2.boundingRect(contours[largest_contour_idx])
+
+                    # Determine padding dynamically based on the dimensions of the bounding box
+                    padding_x = min(w // 2, img_np.shape[1] - w)
+                    padding_y = min(h // 2, img_np.shape[0] - h)
+
+                    # Crop region of interest (ROI) from the original image with adaptive padding
+                    roi = img_np[max(y - padding_y, 0):min(y + h + padding_y, img_np.shape[0]),
+                                 max(x - padding_x, 0):min(x + w + padding_x, img_np.shape[1])]
+
+                    orb = cv2.ORB_create()
+
+                    # Compute keypoints and descriptors for the ROI
+                    kp1, des1 = orb.detectAndCompute(roi, None)
+
+                    for pokemon_file in pokemon_files:
+                        pokemon_name, _ = os.path.splitext(pokemon_file)
+                        stored_img_path = os.path.join(self.image_folder, pokemon_file)
+
+                        # Remove sRGB profile
+                        self.remove_srgb_profile(stored_img_path)
+
+                        stored_img = cv2.imread(stored_img_path)
+
+                        if stored_img is None:
+                            logger.warning(f"Unable to read image: {stored_img_path}")
+                            continue
+
+                        # Ensure correct color format for the stored image
+                        stored_img = self.ensure_correct_color_format(stored_img)
+
+                        # Resize the stored image to match the ROI size for comparison
+                        resized_stored_img = cv2.resize(stored_img, (roi.shape[1], roi.shape[0]))
+
+                        # Compute keypoints and descriptors for the stored image
+                        kp2, des2 = orb.detectAndCompute(resized_stored_img, None)
+
+                        if des1 is not None and des2 is not None:
+                            # Use BFMatcher to find matches between descriptors
+                            bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+                            matches = bf.match(des1, des2)
+                            matches = sorted(matches, key=lambda x: x.distance)
+
+                            # Calculate a score based on the matches
+                            match_score = sum([match.distance for match in matches]) / len(matches)
+
+                            # Update best match if criteria are met
+                            if match_score < highest_score or highest_score == float('-inf'):
+                                highest_score = match_score
+                                best_match = pokemon_name
+
+                            logger.debug(f"Comparing {pokemon_name} with match score: {match_score:.2f}")
+
+                if highest_score > 0:
+                    logger.info(f"Best match: {best_match} with score {highest_score:.2f}")
+                else:
+                    logger.info("No good match found")
+
+                # Draw rectangle around the largest contour
+                if largest_contour_idx != -1:
+                    cv2.rectangle(img_np, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                # Save the color ROI and the best matching stored image for visual comparison
+                if best_match:
+                    matched_img_path = os.path.join(self.image_folder, best_match + ".png")
+                    matched_img = cv2.imread(matched_img_path)
+                    matched_img = self.ensure_correct_color_format(matched_img)
+                    resized_matched_img = cv2.resize(matched_img, (roi.shape[1], roi.shape[0]))
+
+                    # Save the images
+                    os.makedirs("Data", exist_ok=True)
+                    cv2.imwrite("Data/roi.png", roi)
+                    cv2.imwrite("Data/matched_img.png", resized_matched_img)
+
+                    # Create a combined image for better visualization
+                    combined_img = np.hstack((roi, resized_matched_img))
+                    combined_img_path = "Data/combined_comparison.png"
+                    cv2.imwrite(combined_img_path, combined_img)
+
+                    # Send the combined image
+                    await ctx.send(file=discord.File(combined_img_path))
+
+                # Save and send the detected objects image
+                detected_objects_path = "Data/detected_objects.png"
+                cv2.imwrite(detected_objects_path, img_np)
+                await ctx.send(file=discord.File(detected_objects_path))
+
+                return best_match, highest_score
+
+        except Exception as e:
+            logger.error(f"An error occurred during prediction: {e}")
+            return None, 0  
+        
+        
+    def calculate_ssim(self, img1, img2, win_size=7):
+        try:
+            ssim_score, _ = ssim(img1, img2, win_size=win_size, channel_axis=-1, full=True)
+            logger.debug(f"Calculated SSIM score: {ssim_score:.2%}")
+            return ssim_score
+        except Exception as e:
+            logger.error(f"Error calculating SSIM: {e}")
+            return None
 
 
     @commands.command(name='predict')
