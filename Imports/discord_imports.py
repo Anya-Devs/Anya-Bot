ca4e2c4577ef99ab42b84bb3a44e120c90ed8d65
Neko Girl <166271770+cringe-neko-girl@users.noreply.github.com>
2024-07-16 02:44:15 -0400

    Update discord_imports.py

--- Imports/discord_imports.py
+++ Imports/discord_imports.py
@@ -1,26 +1,25 @@
 # Discord Imports
 import discord
-from discord import File, Reaction, User, FFmpegPCMAudio, Activity, ActivityType
-from discord.ext import commands, tasks
-from discord.ui import Select, View, button
-from discord.ext.commands import BucketType, Greedy, Context, check, cooldown
-from discord import app_commands
-from discord.ext.commands import Bot, Cog, command, group, has_permissions, has_role
-from discord.enums import Status, ChannelType
-from discord.errors import NotFound, HTTPException, Forbidden
-from discord.ext.commands import BadArgument, CommandError, CommandNotFound
-from discord.ext.commands import MemberConverter, RoleConverter, TextChannelConverter
-from discord.ext.commands import EmojiConverter, ColorConverter
-from discord.ext.commands import Paginator, HelpCommand, DefaultHelpCommand
-from discord.ext.commands import NoPrivateMessage, NotOwner, MissingPermissions
-from discord.ext.commands import BotMissingPermissions, MissingRole, BotMissingRole
-from discord.ext.commands import UserInputError, CommandInvokeError, CheckFailure
-from discord.ext.commands import ExtensionError, ExtensionNotFound, ExtensionFailed
-from discord.ext.commands import CommandOnCooldown, MaxConcurrencyReached
-from discord.ext.commands import when_mentioned, when_mentioned_or
-from discord.ext.commands import clean_content
-from discord.ext.commands import Group, Context
-from discord.ext.commands import command, group, check, cooldown, bot_has_permissions
+from discord.ext import commands
+from discord.ext.commands import (
+    BucketType, Context, Greedy, bot_has_permissions, check, cooldown, group, has_permissions, has_role
+)
+from discord.enums import ChannelType, Status
+from discord.errors import Forbidden, HTTPException, NotFound
+from discord.ext.commands import (
+    BadArgument, BotMissingPermissions, BotMissingRole, CheckFailure, CommandError, CommandInvokeError,
+    CommandNotFound, CommandOnCooldown, ExtensionError, ExtensionFailed, ExtensionNotFound, MaxConcurrencyReached,
+    MissingPermissions, MissingRole, NoPrivateMessage, NotOwner, UserInputError
+)
+from discord.ext.commands.converter import (
+    ColorConverter, EmojiConverter, MemberConverter, RoleConverter, TextChannelConverter
+)
+
+from discord.ui import Select, View, Button
+from discord.utils import get
+
 
 # Discord Slash Commands
-from typing import Literal, Optional
+from discord import app_commands, TextChannel, Thread
+from typing import Literal, Optional, List, Dict, Union
+
