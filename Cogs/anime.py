4c6350d4d1be8037b636881da0a19bfc5c734649
フォクシー | Senko#4703 <73608888+yakunitatta-senko@users.noreply.github.com>
2024-09-18 02:02:37 -0400

    Update anime.py

--- Cogs/anime.py
+++ Cogs/anime.py
@@ -1,18 +1,15 @@
-# Run Request
 import asyncio
 import requests
 import aiohttp
-
-# Local
-from Imports.log_imports import *
-from Imports.discord_imports import *
+import discord
+from discord.ext import commands
 
 class Anime(commands.Cog):
-  def __init__(self, bot):
+    def __init__(self, bot):
         self.bot = bot
 
-  @commands.command(name='anime')
-  async def anime_search(self, ctx, *, query=None):
+    @commands.command(name='anime')
+    async def anime_search(self, ctx, *, query=None):
         if query is None:
             embed = discord.Embed(description=f"{ctx.author.display_name.title()}, can you try entering the `name` of the anime you're looking for?")
             message = await ctx.reply(embed=embed)
@@ -62,7 +59,58 @@ class Anime(commands.Cog):
             logging.error(f"An unexpected error occurred: {e}")
             await ctx.send(f"An unexpected error occurred: {e}")
 
-  class AnimeButton(discord.ui.Button):
+    @commands.command(name='character')
+    async def character_search(self, ctx, *, query=None):
+        if query is None:
+            embed = discord.Embed(description=f"{ctx.author.display_name.title()}, can you try entering the `name` of the character you're looking for?")
+            message = await ctx.reply(embed=embed)
+
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+
+            try:
+                user_message = await self.bot.wait_for('message', timeout=60, check=check)
+                await message.delete()
+
+                if not user_message.content.strip():
+                    raise ValueError("Character name cannot be empty")
+
+                query = user_message.content.strip()
+                await user_message.delete()
+
+            except asyncio.TimeoutError:
+                timeout_embed = discord.Embed(description="Time's Up! You didn't provide a character name for me to look up.", color=discord.Color.red())
+                await message.edit(embed=timeout_embed)
+                return
+
+            except ValueError as e:
+                error_embed = discord.Embed(description=f"Error: {str(e)}", color=discord.Color.red())
+                await message.edit(embed=error_embed)
+                return
+
+        url = f'https://api.jikan.moe/v4/characters?q={query}'
+
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    response.raise_for_status()
+                    character_data = await response.json()
+
+            current_index = 0
+            current_page = 0
+
+            view = self.CharacterView(character_data, current_index, current_page)
+            embed = await view.update_embed()
+            message = await ctx.reply(embed=embed, view=view)
+
+        except aiohttp.ClientError as e:
+            logging.error(f"An HTTP error occurred: {e}")
+            await ctx.send(f"An HTTP error occurred: {e}")
+        except Exception as e:
+            logging.error(f"An unexpected error occurred: {e}")
+            await ctx.send(f"An unexpected error occurred: {e}")
+
+    class AnimeButton(discord.ui.Button):
         def __init__(self, label, style, custom_id, anime_data, current_index, current_page, max_pages):
             super().__init__(label=label, style=style, custom_id=custom_id)
             self.anime_data = anime_data
@@ -76,20 +124,16 @@ class Anime(commands.Cog):
             elif self.custom_id == "next":
                 self.current_page += 1
 
-            # Ensure that the page index is within bounds
             self.current_page = max(0, min(self.current_page, self.max_pages))
-
-            # Update the current index based on the current page
             self.current_index = self.current_page
 
-            # Create a new AnimeView with the updated page
             view = Anime.AnimeView(self.anime_data, self.current_index, self.current_page)
             embed = await view.update_embed()
             await interaction.response.edit_message(embed=embed, view=view)
 
-  class AnimeView(discord.ui.View):
+    class AnimeView(discord.ui.View):
         def __init__(self, anime_data, current_index, current_page):
-            super().__init__(timeout=200)
+            super().__init__(timeout=None)
             self.anime_data = anime_data
             self.current_index = current_index
             self.current_page = current_page
@@ -121,7 +165,7 @@ class Anime(commands.Cog):
                       f"{'▰' * int(anime['score'] * 10 / 10)}{'▱' * (10 - int(anime['score'] * 10 / 10))}```",
                 inline=False
             )
-            embed.description = "{}".format(anime['synopsis'] if anime and anime.get('synopsis') is not None else "> Synopsis not available"
+            embed.description = anime.get('synopsis', "> <:anya_angy:1268976144548630608> Synopsis not available")
             embed.set_footer(text=f"Page {self.current_page + 1}/{self.max_pages + 1}")
 
             return embed
@@ -138,7 +182,61 @@ class Anime(commands.Cog):
                     return image_url
             return None
 
+    class CharacterButton(discord.ui.Button):
+        def __init__(self, label, style, custom_id, character_data, current_index, current_page, max_pages):
+            super().__init__(label=label, style=style, custom_id=custom_id)
+            self.character_data = character_data
+            self.current_index = current_index
+            self.current_page = current_page
+            self.max_pages = max_pages
+
+        async def callback(self, interaction: discord.Interaction):
+            if self.custom_id == "previous":
+                self.current_page -= 1
+                self.current_index = (self.current_index - 1) % len(self.character_data['data'])
+            elif self.custom_id == "next":
+                self.current_page += 1
+                self.current_index = (self.current_index + 1) % len(self.character_data['data'])
+            
+            view = Anime.CharacterView(self.character_data, self.current_index, self.current_page)
+            embed = await view.update_embed()
+            await interaction.response.edit_message(embed=embed, view=view)
+
+    class CharacterView(discord.ui.View):
+        def __init__(self, character_data, current_index, current_page):
+            super().__init__(timeout=None)
+            self.character_data = character_data
+            self.current_index = current_index
+            self.current_page = current_page
+            self.max_pages = len(character_data['data']) - 1
+            self.update_buttons()
+
+        def update_buttons(self):
+            self.clear_items()
+            if self.current_page > 0:
+                self.add_item(Anime.CharacterButton("Previous", discord.ButtonStyle.primary, "previous", self.character_data, self.current_index, self.current_page, self.max_pages))
+            if self.current_page < self.max_pages:
+                self.add_item(Anime.CharacterButton("Next", discord.ButtonStyle.primary, "next", self.character_data, self.current_index, self.current_page, self.max_pages))
+
+        async def update_embed(self):
+            character = self.character_data['data'][self.current_index]
+            embed = discord.Embed(title=character['name'], url=character['url'])
+
+            image_url = character['images']['jpg'].get('image_url')
+            if image_url:
+                embed.set_image(url=image_url)
+
+            embed.add_field(
+                name="__Details__",
+                value=f"**Nicknames:** `{', '.join(character.get('nicknames', ['None']))}`\n" or ''
+                      f"**Favorites:** `{character.get('favorites', 'N/A')}`\n" or ''
+                      f"**MyAnimeList ID:** `{character.get('mal_id', 'N/A')}`", or ''
+                inline=False
+            )
+            embed.description = character.get('about', "> <:anya_angy:1268976144548630608> Description not available")
+            embed.set_footer(text=f"Page {self.current_page + 1}/{self.max_pages + 1}")
+
+            return embed
+
 def setup(bot):
     bot.add_cog(Anime(bot))
-
-    
