1704e408253bdcf788d8ca829d182100c2cb1e54
cringe-neko-girl <166271770+cringe-neko-girl@users.noreply.github.com>
2024-11-20 22:00:46 -0500

    Update anime.py

--- Cogs/anime.py
+++ Cogs/anime.py
@@ -4,6 +4,15 @@ import aiohttp
 import discord
 from discord.ext import commands
 
+import logging
+
+# Set up logging configuration
+logging.basicConfig(
+    level=logging.ERROR,  # Set the logging level (can be DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # Format of the log messages
+    datefmt='%Y-%m-%d %H:%M:%S',  # Date and time format
+)
+
 class Anime(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -101,7 +110,7 @@ class Anime(commands.Cog):
 
             view = self.CharacterView(character_data, current_index, current_page)
             embed = await view.update_embed()
-            message = await ctx.reply(embed=embed, view=view)
+            message = await ctx.reply(embed=embed, view=view, mention_author=False)
 
         except aiohttp.ClientError as e:
             logging.error(f"An HTTP error occurred: {e}")
@@ -109,7 +118,61 @@ class Anime(commands.Cog):
         except Exception as e:
             logging.error(f"An unexpected error occurred: {e}")
             await ctx.send(f"An unexpected error occurred: {e}")
+            
+    @commands.command(name='manga')
+    async def manga_search(self, ctx, *, query=None):
+        if query is None:
+            embed = discord.Embed(description=f"{ctx.author.display_name.title()}, can you try entering the `name` of the manga you're looking for?")
+            message = await ctx.reply(embed=embed, mention_author=False)
 
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+
+            try:
+                user_message = await self.bot.wait_for('message', timeout=60, check=check)
+                await message.delete()
+
+                if not user_message.content.strip():
+                    raise ValueError("Manga title cannot be empty")
+
+                query = user_message.content.strip()
+                await user_message.delete()
+
+            except asyncio.TimeoutError:
+                timeout_embed = discord.Embed(description="Time's Up! You didn't provide a manga title for me to look up.", color=discord.Color.red())
+                await message.edit(embed=timeout_embed)
+                return
+
+            except ValueError as e:
+                error_embed = discord.Embed(description=f"Error: {str(e)}", color=discord.Color.red())
+                await message.edit(embed=error_embed)
+                return
+
+        url = f'https://api.jikan.moe/v4/manga?q={query}'
+
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    response.raise_for_status()
+                    manga_data = await response.json()
+
+            if not manga_data['data']:
+                await ctx.reply(f"No results found for '{query}'. Please try a different title.")
+                return
+
+            current_index = 0
+            current_page = 0
+
+            view = self.MangaView(manga_data, current_index, current_page)
+            embed = await view.update_embed()
+            await ctx.reply(embed=embed, view=view, mention_author=False)
+
+        except aiohttp.ClientError as e:
+            logging.error(f"An HTTP error occurred: {e}")
+            await ctx.send(f"An HTTP error occurred: {e}")
+        except Exception as e:
+            logging.error(f"An unexpected error occurred: {e}")
+            await ctx.send(f"An unexpected error occurred: {e}")
     class AnimeButton(discord.ui.Button):
         def __init__(self, label, style, custom_id, anime_data, current_index, current_page, max_pages):
             super().__init__(label=label, style=style, custom_id=custom_id)
@@ -151,9 +214,9 @@ class Anime(commands.Cog):
             anime = self.anime_data['data'][self.current_index]
             embed = discord.Embed(title=anime['title'])
 
-            thumbnail_url = self.get_image_url(anime['images'])
-            if thumbnail_url:
-                embed.set_thumbnail(url=thumbnail_url)
+            image_url = self.get_image_url(anime['images'])
+            if image_url:
+                embed.set_thumbnail(url=image_url)
 
             embed.add_field(
                 name=" ",
@@ -181,7 +244,7 @@ class Anime(commands.Cog):
                 if image_url:
                     return image_url
             return None
-
+   
     class CharacterButton(discord.ui.Button):
         def __init__(self, label, style, custom_id, character_data, current_index, current_page, max_pages):
             super().__init__(label=label, style=style, custom_id=custom_id)
@@ -238,6 +301,89 @@ class Anime(commands.Cog):
             embed.set_footer(text=f"ID: {character.get('mal_id', 'N/A')}\t\t\tPage {self.current_page + 1}/{self.max_pages + 1}")
 
             return embed
+        
+    class MangaButton(discord.ui.Button):
+     def __init__(self, label, style, action):
+        super().__init__(label=label, style=style)
+        self.action = action
+
+     async def callback(self, interaction: discord.Interaction):
+        # Access the parent view (MangaView)
+        view = self.view
+
+        if self.action == "previous" and view.current_page > 0:
+            view.current_page -= 1
+        elif self.action == "next" and view.current_page < view.max_pages:
+            view.current_page += 1
+
+        # Update the current index based on the new page
+        view.current_index = view.current_page
+
+        # Update the buttons and the embed
+        view.update_buttons()
+        embed = await view.update_embed()
+        await interaction.response.edit_message(embed=embed, view=view)
+
+ 
+    class MangaView(discord.ui.View):
+     def __init__(self, manga_data, current_index=0, current_page=0, timeout=None):
+        super().__init__(timeout=timeout)
+        self.manga_data = manga_data
+        self.current_index = current_index
+        self.current_page = current_page
+        self.max_pages = len(manga_data['data']) - 1
+        self.update_buttons()
+
+     def update_buttons(self):
+        self.clear_items()  # Remove all current buttons
+        # Create the buttons and add them to the view based on the current page
+        if self.current_page > 0:
+            self.add_item(Anime.MangaButton("Previous", discord.ButtonStyle.primary, "previous"))
+        if self.current_page < self.max_pages:
+            self.add_item(Anime.MangaButton("Next", discord.ButtonStyle.primary, "next"))
+
+     async def update_embed(self):
+        manga = self.manga_data['data'][self.current_index]
+        embed = discord.Embed(title=manga['title'])
+
+        # Get the appropriate image URL (we'll prioritize large size)
+        image_url = self.get_image_url(manga['images'])
+        if image_url:
+            embed.set_image(url=image_url)
+
+        # Add additional fields such as chapters, status, and score
+        embed.add_field(
+            name=" ",
+            value=f"**Chapters:** `{manga['chapters']}`\n"
+                  f"**Status:** `{manga['status']}`\n"
+                  f"**Genres:** `{', '.join(genre['name'] for genre in manga['genres'])}`\n"
+                  f"```py\nScore: {manga['score'] or 'N/A'}\n"
+                  f"{'▰' * int(manga.get('score', 0) * 10 / 10)}{'▱' * (10 - int(manga.get('score', 0) * 10 / 10))}```",
+            inline=False
+        )
+
+        # Display synopsis or default message if not available
+        embed.description = manga.get('synopsis', "> <:anya_angy:1268976144548630608> Synopsis not available")
+        embed.set_footer(text=f"Page {self.current_page + 1}/{self.max_pages + 1}")
+
+        return embed
+
+     def get_image_url(self, images):
+        size_order = ['large', 'medium', 'small']
+        for size in size_order:
+            image_url = images.get('jpg', {}).get(f'{size}_image_url')
+            if image_url:
+                return image_url
+        for size in size_order:
+            image_url = images.get('webp', {}).get(f'{size}_image_url')
+            if image_url:
+                return image_url
+        return None
+
+
+
+
+
 
 def setup(bot):
     bot.add_cog(Anime(bot))
