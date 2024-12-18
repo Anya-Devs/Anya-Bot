0eeed614fc4a2601e340f0c3e803a3958cbcb2d1
cringe-neko-girl <166271770+cringe-neko-girl@users.noreply.github.com>
2024-12-18 19:50:39 +0000

    updated main

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class BotSetup(commands.AutoShardedBot):
         self.mongoConnect = None
         self.DB_NAME = 'Bot'
         self.COLLECTION_NAME = 'information'
-        self.token_type = "Test_Token"
+        self.token_type = "Token"
 
     async def on_ready(self):
         print(f"\033[92mLogged in as {self.user} (ID: {self.user.id})\033[0m")
