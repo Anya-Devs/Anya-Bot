a8adfd48bb616bab29687fd083503cd4ac20fa03
cringe-neko-girl <166271770+cringe-neko-girl@users.noreply.github.com>
2025-02-25 11:26:08 -0500

    Update log_imports.py

--- Imports/log_imports.py
+++ Imports/log_imports.py
@@ -2,14 +2,11 @@ import logging
 import colorlog
 import colorama
 
-# Initialize colorama
 colorama.init()
 
-# Set up logging with color
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)  # Set logging level to DEBUG
+logger.setLevel(logging.DEBUG)  
 
-# Create console handler and set level to DEBUG
 handler = colorlog.StreamHandler()
 handler.setFormatter(
     colorlog.ColoredFormatter(
@@ -25,7 +22,6 @@ handler.setFormatter(
 )
 logger.addHandler(handler)
 
-# Example usage of the logger
 logger.debug("This is a debug message")
 logger.info("This is an info message")
 logger.warning("This is a warning message")
