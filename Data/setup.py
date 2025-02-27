e77bf18d29ab51849be2d3249dbcd0ce1d9d322d
cringe-neko-girl <166271770+cringe-neko-girl@users.noreply.github.com>
2025-02-27 06:08:19 -0500

    Update setup.py

--- Data/setup.py
+++ Data/setup.py
@@ -3,15 +3,14 @@ os.system("pip install pipreqs")
 import subprocess
 
 
-# Function to install a package
 def install_package(package):
     subprocess.run(['pip', 'install', '--quiet', package], check=True)
 
-# Function to generate requirements.txt
+
 def generate_requirements():
     subprocess.run(['pipreqs', '--force', '--ignore', 'venv,.venv', '.'], check=True)
 
-# Main function to generate and install necessary dependencies
+
 def start():
     generate_requirements()
     install_package('pipreqs')
