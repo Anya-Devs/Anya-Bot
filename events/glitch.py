9ff8e55a9d6e3709616f44119c66639115632d0a
cringe-neko-girl <166271770+cringe-neko-girl@users.noreply.github.com>
2025-04-01 05:14:26 -0400

    Update glitch.py

--- events/glitch.py
+++ events/glitch.py
@@ -4,60 +4,61 @@ import cv2, numpy as np, requests, os, itertools, logging
 
 
 class ImgPuzzle:
-    def __init__(self, url, w=800):
-        self.u, self.w, self.i, self.p, self.s = url, w, None, [], {}
-        self.image_dir = "Data/commands/pokemon/events/glitched/images"  
+    def __init__(s, url, w=800):
+        s.u, s.w, s.i, s.p, s.s = url, w, None, [], {}
 
-    def load(self):
-        os.makedirs(self.image_dir, exist_ok=True)
-
+    def load(s):
         try:
-            r = requests.get(self.u, stream=True)
+            r = requests.get(s.u, stream=True)
             r.raise_for_status()
             a = np.asarray(bytearray(r.content), dtype=np.uint8)
             i = cv2.imdecode(a, cv2.IMREAD_COLOR)
         except requests.RequestException:
             i = cv2.imread("test_image.png", cv2.IMREAD_COLOR)
-
-        if i is None:
-            raise ValueError("Image load failed")
+        if i is None: raise ValueError("Image load failed")
         ar = i.shape[1] / i.shape[0]
-        self.i = cv2.resize(i, (self.w, int(self.w / ar)))
+        s.i = cv2.resize(i, (s.w, int(s.w / ar)))
 
-    def split(self):
-        h, w = self.i.shape[:2]
+    def split(s):
+        h, w = s.i.shape[:2]
         mx, my = w // 2, h // 2
-        self.p = [self.i[:my, :mx], self.i[:my, mx:], self.i[my:, :mx], self.i[my:, mx:]]
-        
-        [cv2.imwrite(os.path.join(self.image_dir, f"part_{chr(65+i)}.png"), p) for i, p in enumerate(self.p)]
+        s.p = [s.i[:my, :mx], s.i[:my, mx:], s.i[my:, :mx], s.i[my:, mx:]]
+        os.makedirs("pieces", exist_ok=True)
+        [cv2.imwrite(f"pieces/{chr(65+i)}.png", p) for i, p in enumerate(s.p)]
 
-    def check(self, i):
+    def check(s, i):
+        orb = cv2.ORB_create()
         g = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
+        kp, des = orb.detectAndCompute(g, None)
         _, t = cv2.threshold(g, 100, 255, cv2.THRESH_BINARY)
         c, _ = cv2.findContours(t, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        return sum(cv2.contourArea(x) for x in c)
 
-    def eval(self, perm):
-        ni = np.zeros_like(self.i)
+        return len(kp), sum(cv2.contourArea(x) for x in c)
+
+    def eval(s, perm):
+        ni = np.zeros_like(s.i)
         h, w = ni.shape[:2]
         mx, my = w // 2, h // 2
         ps = [(0, 0), (0, mx), (my, 0), (my, mx)]
         for idx, p in enumerate(perm):
             y, x = ps[idx]
-            ni[y:y+my, x:x+mx] = self.p[p]
-        sc = self.check(ni)
-        return sc, (sc / (h * w)) * 100
+            ni[y:y+my, x:x+mx] = s.p[p]
+        sc, f = s.check(ni)
+        return sc + f, (sc / (h * w)) * 100 
 
-    def solve(self):
-        self.load()
-        self.split()
+    def solve(s):
+        s.load()
+        s.split()
         lb, bp, bs, bc = ['A', 'B', 'C', 'D'], None, 0, 0
         for p in itertools.permutations(range(4)):
-            sc, c = self.eval(p)
-            self.s[p] = (sc, c)
-            if sc > bs:
-                bp, bs, bc = p, sc, c
+            sc, c = s.eval(p)
+            s.s[p] = (sc, c)
+            if sc > bs: bp, bs, bc = p, sc, c
         return ''.join(lb[i] for i in bp)
+    
+
+
+
 class GlitchSolver(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
