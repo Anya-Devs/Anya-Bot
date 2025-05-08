7d5f69b900aecb496fe61173b18a8dec933d2d6b
cringe-neko-girl <166271770+cringe-neko-girl@users.noreply.github.com>
2025-05-08 09:17:58 +0000

    added action commands

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -3,16 +3,18 @@
 # hug, iq, kiss, lick, nervous, pat, pinch, poke, pp, simprate, slap,
 # slowclap, smile, smug, slot, strength, waifurate, wave, wink
 
+import types
+from motor.motor_asyncio import AsyncIOMotorClient
+
 from utils.cogs.fun import *
 from imports.discord_imports import *
 from data.commands.fun.emojis import *
-
-
 
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.fun_cmd = Fun_Commands()
+        self.create_action_commands()
 
     @commands.command(name='8ball')
     async def eight_ball(self, ctx, *, question):
@@ -20,15 +22,17 @@ class Fun(commands.Cog):
         response_build = f'**:8ball: | {ctx.author.display_name} asked:** *"{question}"*\n{blank_emoji} | **Answer:** {prediction}'
         await ctx.reply(response_build, mention_author=False)
 
-    @commands.command(name='pat')
-    async def pat(self, ctx, user: Union[discord.Member, Literal["everyone"]]):
-        pass
+    def create_action_commands(self):
+     actions = ['pat', 'cuddle', 'bite', 'kiss', 'lick', "hug"]
+     for action in actions:
+        @commands.command(name=action)
+        async def action_fn(ctx, user: Union[discord.Member, Literal["everyone"]] = None, *, additional_text: str = ""):
+            if user is None: user = ctx.author
+            embed = await  self.fun_cmd.action_command(ctx, user, additional_text)
+            await ctx.send(embed=embed)
+        action_fn.__name__ = action 
+        self.bot.add_command(action_fn)
 
 
-
- 
-
-   
-
-def setup(bot):
-    bot.add_cog(Fun)
+async def setup(bot):
+    await bot.add_cog(Fun(bot))
--- utils/cogs/fun.py
+++ utils/cogs/fun.py
@@ -3,17 +3,39 @@
 # hug, iq, kiss, lick, nervous, pat, pinch, poke, pp, simprate, slap,
 # slowclap, smile, smug, slot, strength, waifurate, wave, wink
 
-import aiofiles, random
+from data.const import *
+import aiofiles, random, aiohttp
 from imports.discord_imports import *
+from motor.motor_asyncio import AsyncIOMotorClient
+
 
 
 
 class Fun_Commands:
     def __init__(self):
         self._8ball_file = "data/commands/fun/8ball-responses.txt"
-    
+        self.action_command_api = "https://api.otakugifs.xyz/gif?reaction={}"
+        self.mongo = AsyncIOMotorClient(os.getenv("MONGO_URI")).Commands.fun
+
     async def eight_ball(self):
         async with aiofiles.open(self._8ball_file, mode="r") as file:
             responses = await file.readlines()
-        response = random.choice([line.strip() for line in responses if line.strip()])
-        return response
+        return random.choice([r.strip() for r in responses if r.strip()])
+
+    async def action_command(self, ctx, user: Union[discord.Member, Literal["everyone"]], additional_text=""):
+        action = ctx.command.name
+        async with aiohttp.ClientSession() as s:
+            gif = (await (await s.get(self.action_command_api.format(action))).json())["url"]
+        emotes = {"<3": ["pat", "cuddle", "kiss", "hug"], ">:0": ["bite", "lick"]}
+        emote = next((e for e, acts in emotes.items() if action in acts), "")
+        everyone = isinstance(user, str) and user.lower() == "everyone"
+        msg = f"{ctx.author.display_name} {action}s the whole server {emote} {additional_text}" if everyone else f"{ctx.author.display_name} {action}s {user.display_name} {emote} {additional_text}"
+        embed = discord.Embed(title=msg.strip(), color=primary_color()).set_image(url=gif)
+        if not everyone and user != ctx.author:
+            sid, aid, uid = str(ctx.guild.id), str(ctx.author.id), str(user.id)
+            await self.mongo.update_one({"server_id": sid}, {"$inc": {f"members.outake.{aid}": 1, f"members.intake.{uid}": 1}}, upsert=True)
+            doc = await self.mongo.find_one({"server_id": sid})
+            outake = doc.get("members", {}).get("outake", {}).get(aid, 0)
+            intake = doc.get("members", {}).get("intake", {}).get(uid, 0)
+            embed.set_footer(text=f"Sent: {outake} | Received: {intake}")
+        return embed
