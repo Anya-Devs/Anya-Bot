a6f79fb3cda4aae74bc558ba8ddc4d768b0247eb
cringe-neko-girl <166271770+cringe-neko-girl@users.noreply.github.com>
2025-05-09 07:07:28 +0000

    updated fun

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -1,39 +1,46 @@
-from utils.cogs.fun import *
-from imports.discord_imports import *
-from data.commands.fun.emojis import *
-from typing import Union, Literal
+from utils.cogs.fun import *; from imports.discord_imports import *; from data.commands.fun.emojis import *; from typing import Union, Literal
 
 class Fun(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, bot): 
+        self.bot = bot 
         self.fun_cmd = Fun_Commands()
         self._dynamic_commands = []
+        self.action_commands_response_path ='data/commands/fun/action-response.json'
         self.create_action_commands()
 
     @commands.command(name='8ball')
-    async def eight_ball(self, ctx, *, question):
-        prediction = await self.fun_cmd.eight_ball()
-        response_build = f'**:8ball: | {ctx.author.display_name} asked:** *"{question}"*\n{blank_emoji} | **Answer:** {prediction}'
-        await ctx.reply(response_build, mention_author=False)
+    async def eight_ball(self, ctx, *, question): 
+        await ctx.reply(f'**:8ball: | {ctx.author.display_name} asked:** *"{question}"*\n{blank_emoji} | **Answer:** {await self.fun_cmd.eight_ball()}', mention_author=False)
 
     def create_action_commands(self):
-        actions = ['pat', 'cuddle', 'bite', 'kiss', 'lick', "hug", "cry", "wave", "slowclap", "smug"]
+     actions = ['pat', 'cuddle', 'bite', 'kiss', 'lick', 'hug', 'cry', 'wave', 'slowclap', 'smug', 'dance', 'happy']
+     for action in actions:
+        async def action_fn(ctx, user: Union[discord.Member, Literal["everyone"]] = None, *, additional_text: str = ""):
+            embed, msg = await self.fun_cmd.action_command(ctx, user or ctx.author, additional_text)
+            await (ctx.reply(msg, mention_author=False) if embed is None else ctx.send(embed=embed))
+        action_fn.__name__ = action
+        command = commands.Command(action_fn)
+        self._dynamic_commands.append(command)
+        self.bot.add_command(command)
+     self.add_missing_actions_to_json(actions)
 
-        for action in actions:
-            async def action_fn(ctx, user: Union[discord.Member, Literal["everyone"]] = None, *, additional_text: str = ""):
-                if user is None:
-                    user = ctx.author
-                embed = await self.fun_cmd.action_command(ctx, user, additional_text)
-                await ctx.send(embed=embed)
+    def add_missing_actions_to_json(self, actions):
+        try:
+            with open(self.action_commands_response_path, 'r+') as f:
+                action_data = json.load(f)
+                missing_actions = set(actions) - set(action_data["phrases"]["self"].keys())
+                for missing_action in missing_actions:
+                    action_data["phrases"]["self"][missing_action] = f"{missing_action}s themselves"
+                    action_data["phrases"]["other"][missing_action] = f"{missing_action} {action_data['phrases']['other'].get(missing_action, '{target}')}"
+                    action_data["phrases"]["everyone"][missing_action] = f"{missing_action} the whole server"
+                f.seek(0)
+                json.dump(action_data, f, indent=4)
+                f.truncate()
+        except FileNotFoundError:
+            pass
 
-            action_fn.__name__ = action
-            command = commands.Command(action_fn)
-            self._dynamic_commands.append(command)
-            self.bot.add_command(command)
-
-    def get_commands(self):
+    def get_commands(self): 
         return super().get_commands() + self._dynamic_commands
-
 
 async def setup(bot):
     await bot.add_cog(Fun(bot))
--- utils/cogs/fun.py
+++ utils/cogs/fun.py
@@ -14,8 +14,13 @@ from motor.motor_asyncio import AsyncIOMotorClient
 class Fun_Commands:
     def __init__(self):
         self._8ball_file = "data/commands/fun/8ball-responses.txt"
+        action_command_file = "data/commands/fun/action-response.json"
         self.action_command_api = "https://api.otakugifs.xyz/gif?reaction={}"
         self.mongo = AsyncIOMotorClient(os.getenv("MONGO_URI")).Commands.fun
+
+        with open(action_command_file) as f: self.action_data = json.load(f)
+        self.emotes = self.action_data["emotes"]
+        self.phrases = self.action_data["phrases"]
 
     async def eight_ball(self):
         async with aiofiles.open(self._8ball_file, mode="r") as file:
@@ -24,17 +29,20 @@ class Fun_Commands:
 
     async def action_command(self, ctx, user: Union[discord.Member, Literal["everyone"]], additional_text=""):
      action = ctx.command.name
-     async with aiohttp.ClientSession() as s: 
-      gif = (await (await s.get(self.action_command_api.format(action))).json())["url"]
-      emotes = {"<3": ["pat", "cuddle", "kiss", "hug"], ">:0": ["bite", "lick", "slap"], ":(": ["cry"], "= w =": ["slowclap", "wave", "smug"]}
-      emote = next((e for e, acts in emotes.items() if action in acts), "")
-      action_phrases = {"pat": "gives a gentle pat to", "cuddle": "cuddles", "kiss": "gives a kiss to", "hug": "hugs", "bite": "bites", "lick": "licks", "slap": "slaps", "cry": "is crying", "slowclap": "gives a slow clap", "wave": "waves at", "smug": "gives a smug look to"}
-      msg = f"{ctx.author.display_name} {action_phrases.get(action, f'{action}s')} {('the whole server' if isinstance(user, str) and user.lower() == 'everyone' else user.display_name)} {emote} {additional_text}"
-      embed = discord.Embed(title=msg.strip(), color=primary_color()).set_image(url=gif)
-      sid, aid = str(ctx.guild.id), str(ctx.author.id)
-      if isinstance(user, discord.Member) and not user.bot and user != ctx.author: await self.mongo.update_one({"server_id": sid}, {"$inc": {f"members.{action}.intake.{str(user.id)}": 1}}, upsert=True)
-      if not ctx.author.bot and not isinstance(user, str) and user != "everyone" and not user.bot: await self.mongo.update_one({"server_id": sid}, {"$inc": {f"members.{action}.outake.{aid}": 1}}, upsert=True)
-      doc = await self.mongo.find_one({"server_id": sid})
-      sent, received = doc.get("members", {}).get(action, {}).get("outake", {}).get(aid, 0), doc.get("members", {}).get(action, {}).get("intake", {}).get(aid, 0)
-      embed.set_footer(text=f"Sent: {sent} | Received: {received}")
-      return embed
+     async with aiohttp.ClientSession() as s:
+        gif = (await (await s.get(self.action_command_api.format(action))).json())["url"]
+     emote = next((e for e, acts in self.emotes.items() if action in acts), "")
+     if isinstance(user, str) and user.lower() == 'everyone': action_target, action_phrase = "the whole server", self.phrases["everyone"].get(action, f"{action}s")
+     elif isinstance(user, discord.Member) and user == ctx.author: action_target, action_phrase = "themselves", self.phrases["self"].get(action, f"{action}s")
+     else: action_target, action_phrase = user.display_name, self.phrases["other"].get(action, f"{action}s")
+     is_plain = "[no_embed]" in action_phrase
+     action_phrase = action_phrase.replace("[no_embed]", "").strip()
+     msg = f"{action_phrase.format(user=ctx.author.display_name, target=action_target)} {emote} {additional_text}".strip()
+     sid, aid = str(ctx.guild.id), str(ctx.author.id)
+     if isinstance(user, discord.Member) and not user.bot and user != ctx.author: await self.mongo.update_one({"server_id": sid}, {"$inc": {f"members.{action}.intake.{str(user.id)}": 1}}, upsert=True)
+     elif user != ctx.author and not ctx.author.bot and not isinstance(user, str) and user != "everyone" and not user.bot: await self.mongo.update_one({"server_id": sid}, {"$inc": {f"members.{action}.outake.{aid}": 1}}, upsert=True)
+     doc = await self.mongo.find_one({"server_id": sid})
+     sent = doc.get("members", {}).get(action, {}).get("outake", {}).get(aid, 0)
+     received = doc.get("members", {}).get(action, {}).get("intake", {}).get(aid, 0)
+     embed = None if is_plain else discord.Embed(title=msg, color=primary_color()).set_image(url=gif).set_footer(text=f"Sent: {sent} | Received: {received}")
+     return embed, msg
