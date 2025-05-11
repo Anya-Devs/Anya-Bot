a5e4683fdf6cb29030891992694639fd10dde633
cringe-neko-girl <166271770+cringe-neko-girl@users.noreply.github.com>
2025-05-11 00:07:09 +0000

    updated fun

--- utils/cogs/fun.py
+++ utils/cogs/fun.py
@@ -32,15 +32,20 @@ class Fun_Commands:
      async with aiohttp.ClientSession() as s:
         gif = (await (await s.get(self.action_command_api.format(action))).json())["url"]
      emote = next((e for e, acts in self.emotes.items() if action in acts), "")
-     if isinstance(user, str) and user.lower() == 'everyone': action_target, action_phrase = "the whole server", self.phrases["everyone"].get(action, f"{action}s")
-     elif isinstance(user, discord.Member) and user == ctx.author: action_target, action_phrase = "themselves", self.phrases["self"].get(action, f"{action}s")
-     else: action_target, action_phrase = user.display_name, self.phrases["other"].get(action, f"{action}s")
+     if isinstance(user, str) and user.lower() == "everyone":
+        action_target, action_phrase = "the whole server", self.phrases["everyone"].get(action, f"{action}s")
+     elif isinstance(user, discord.Member) and user == ctx.author:
+        action_target, action_phrase = "themselves", self.phrases["self"].get(action, f"{action}s")
+     else:
+        action_target, action_phrase = user.display_name, self.phrases["other"].get(action, f"{action}s")
      is_plain = "[no_embed]" in action_phrase
      action_phrase = action_phrase.replace("[no_embed]", "").strip()
      msg = f"{action_phrase.format(user=ctx.author.display_name, target=action_target)} {emote} {additional_text}".strip()
      sid, aid = str(ctx.guild.id), str(ctx.author.id)
-     if isinstance(user, discord.Member) and not user.bot and user != ctx.author: await self.mongo.update_one({"server_id": sid}, {"$inc": {f"members.{action}.intake.{str(user.id)}": 1}}, upsert=True)
-     elif user != ctx.author and not ctx.author.bot and not isinstance(user, str) and user != "everyone" and not user.bot: await self.mongo.update_one({"server_id": sid}, {"$inc": {f"members.{action}.outake.{aid}": 1}}, upsert=True)
+     if isinstance(user, discord.Member) and not user.bot and user != ctx.author:
+        await self.mongo.update_one({"server_id": sid}, {"$inc": {f"members.{action}.intake.{str(user.id)}": 1}}, upsert=True)
+     if (isinstance(user, discord.Member) and not user.bot and user != ctx.author) or (isinstance(user, str) and user.lower() == "everyone"):
+        await self.mongo.update_one({"server_id": sid}, {"$inc": {f"members.{action}.outake.{aid}": 1}}, upsert=True)
      doc = await self.mongo.find_one({"server_id": sid})
      sent = doc.get("members", {}).get(action, {}).get("outake", {}).get(aid, 0)
      received = doc.get("members", {}).get(action, {}).get("intake", {}).get(aid, 0)
